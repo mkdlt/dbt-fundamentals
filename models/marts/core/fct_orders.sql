9788caaa4c990d3fce8aee3a1ddfdbeeb4568141
mikhail@zookal.com <mikhail@zookal.com>
2022-03-01 13:35:08 +0000

    Refactor dim_customers model

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,19 +1,30 @@
-WITH orders AS (
-    
-    SELECT * FROM {{ ref('stg_orders') }}
-
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
-payments AS (
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
 
-    SELECT * FROM {{ ref('stg_payments') }}
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
 
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-SELECT o.customer_id,
-       o.order_id,
-       p.amount
-  FROM orders AS o
-  LEFT
-  JOIN payments AS p
-    ON o.order_id = p.order_id
+select * from final
